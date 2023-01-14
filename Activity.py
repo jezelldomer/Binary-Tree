d20e0d6a5abda3dd994325c1559b1c0a39eab482
jezelldomer <jzlldomer@gmail.com>
2023-01-14 13:20:31 +0800

    Finishing the codes for searching and listing the elements in order

--- Activity.py
+++ Activity.py
@@ -1,10 +1,10 @@
-
 class BinarySearchTreeNode:
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
-        
+
+    #function for child in data
     def add_child(self, data):
         if data == self.data:
             return # node already exist
@@ -19,8 +19,25 @@ class BinarySearchTreeNode:
                 self.right.add_child(data)
             else:
                 self.right = BinarySearchTreeNode(data)
-     
-    #function for In order transversal
+
+    #function for searching elements
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+                
+    #function for in order traversal
     def in_order_traversal(self):
         elements = []
         if self.left:
@@ -31,4 +48,25 @@ class BinarySearchTreeNode:
         if self.right:
             elements += self.right.in_order_traversal()
 
-        return elements           
+        return elements
+
+
+def build_tree(elements):
+    print("Building tree with these elements,", "letters of my Full Name:",elements,"\n")
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+
+if __name__ == '__main__':
+    letters = ["J","E","Z", "E","L","L","D","O","M","E","R"] #elements
+    
+    #for printing the search elements
+    letter_tree = build_tree(letters)
+    print("E is in the list? ", letter_tree.search("E"))
+    print("P is in the list? ", letter_tree.search("P"))
+    #for printing the order traversal
+    print("\nIn order traversal gives this sorted list:",letter_tree.in_order_traversal())
